d7d4e87403b655b6a7f538189a467a1f098aa703
Edward Cochran <edwardcochran23@gmail.com>
2020-03-31 14:37:01 -0400

    Read and Write

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        Login login = new Login();
-        login.printLoginScreen();
-        Employee em = new Employee(false,false);
-        em.getVacationTime();
-        em.setDaysRequestedOff(5);
+//    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws FileNotFoundException {
+//        RecordRW rw = new RecordRW();
+////        User ed = new User("Edward","Cochran");
+////        User c = new User("Carmen")
+//        rw.writeDailyRecord("test");
+//        rw.writeDailyRecord("test2");
+//        Scanner fL = null;
+//        if(rw.fileLookup.exists()){
+//         fL= new Scanner(rw.fileLookup);
+//        }
+//        while(fL.hasNextLine())
+//        {
+//            System.out.println(fL.nextLine());
+//        }
+//        System.out.println("Pick a number");
+//        rw.readSaveFile(in.nextInt());
+//        Login login = new Login();
+//        login.printLoginScreen();
+//        Employee em = new Employee(false,false);
+//        em.getVacationTime();
+//        em.setDaysRequestedOff(5);
+
+
     }
 }
