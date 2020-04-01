66211c236cf5b538ec9a811ee5e458490f6c9110
Carmen <Carmen.smith490@gmail.com>
2020-04-01 14:29:15 -0400

    update on Logger

--- src/Logger.java
+++ src/Logger.java
@@ -1,107 +1,80 @@
 
 
-import java.io.Serializable;
+
 import java.util.*;
 
 public class Logger {
     static Scanner scan = new Scanner(System.in);
-   static HashMap<String , HashMap<String,Boolean>> ctt = new HashMap<>();
-
-
+    static ArrayList<Stu> abc= new ArrayList<>();
     public static void main(String[] args) {
-
         ArrayList<String> rollCall = new ArrayList<>();
         rollCall.add("Johnny");
         rollCall.add("Sarah");
         rollCall.add("Katie");
-        /*rollCall.add("Liam");
-        rollCall.add("Sammy");
-        rollCall.add("Jasmine");
-*/
 
-        // callAttendance(rollCall);
-        isLateAttendance(rollCall);
+        callAttendance(rollCall);
+        latePerson(0);
+
+    }
+
+    static class Stu {   //
+        String name1;
+        String choice;
+        Boolean here;
+        Boolean late ;
 
 
+        public Stu(String c,String m, Boolean a, Boolean b) {
+            this.name1 = c;
+            this.choice = m;
+            this.here = a;
+            this.late = b;
+        }
     }
 
     public static void callAttendance(ArrayList<String> names) {
+
         boolean isHere;
-        Pair<String, Boolean> put;
+        boolean isLate = false;
+
         for (String name : names) {
             System.out.println("Is " + name + " here?");
+            // may have to add another string variable to coincide with first input
             String ans = "";
             while (!ans.equals("yes") && (!ans.equals("no"))) {
                 ans = scan.next();
-                HashMap<String,Boolean> pair= new HashMap<>();
 
                 if (ans.equals("yes")) {
                     isHere = true;
-                    ctt.put(name, isHere);
-                    System.out.println("Was Employee on Time? ");
                     isLate = false;
-                    ans = scan.next();
-                    if (ans.equals("Yes")) {
 
-                    pair.put("Here",isHere);
-                     ctt.put(name,pair);
-                }
-                else if (ans.equals("no")) {
-                    isHere = false;
 
-                    pair.put("Absent",isHere);
-                    ctt.put(name,pair);
+                    abc.add(new Stu (name,"Here", isHere,isLate));
 
-                } else {
-                    System.out.println("Invalid answer, enter yes or no.");
-                    ans = scan.next();
-
-                }
-            }
-        }
-        
-        for(Map.Entry<String,HashMap<String,Boolean>> k: ctt.entrySet()){
-           for(Map.Entry<String,Boolean> l:k.getValue().entrySet()){
-               System.out.println(k.getKey() + " is "+ l.getKey()+".");
-           }
-        }
-    }
-
-    public static void isLateAttendance(ArrayList<String> names) {
-
-        boolean isLate;
-
-        for (String name : names) {
-            System.out.println("Was " + name + " late?");
-            String ans = "";
-            while (!ans.equals("yes") && (!ans.equals("no"))) {
-                ans = scan.next();
-
-                if (ans.equals("yes")) {
-                    isLate = true;
-                    ctt.put(name, isLate);
                 } else if (ans.equals("no")) {
+                    isHere = false;
                     isLate = false;
-                    ctt.put(name, isLate);
 
+                    abc.add(new Stu(name,"Absent", isHere, isLate));
                 } else {
                     System.out.println("Invalid answer, enter yes or no.");
                     ans = scan.next();
-
                 }
             }
+
         }
-        for (var k : ctt.entrySet()) {
-            String name = k.getKey();
-            if (k.getValue()) {
-                System.out.println(name + " was late.");
-            } else {
-                System.out.println(name + " was on time.");
-            }
+
+        for(Stu s : abc){
+
+            System.out.println(s);
         }
 
     }
+    public static void latePerson(int index){
+        abc.set(index,new Stu(abc.get(index).name1,"late", true,true));
+        System.out.println(abc.get(index).name1);
 
+    }
 }
 
 
